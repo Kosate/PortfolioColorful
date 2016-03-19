a7609f5d3bb2afb3b2967d6843e5bcb22a0253a9
neungkl <kosatelim@gmail.com>
2016-03-19 19:35:19 +0700

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp');
+var ejs = require('gulp-ejs');
+var scss = require('gulp-sass');
+var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+var production = true;
+
+gulp.task('static', function() {
+	var bowerList = [
+		'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/pure/pure-min.css'
+	];
+
+	gulp.src([
+		'app/**/*',
+        '!app/bower_components/**/*',
+		'!app/**/*.scss',
+		'!app/**/*.es6',
+		'!app/**/*.ejs',
+		'!app/**/*.tmp'
+	], {base: 'app'})
+		.pipe(gulp.dest('dist/'));
+
+	gulp.src(bowerList.map(function(val) { return 'app/' + val }), {base : 'app/bower_components/'})
+		.pipe(gulp.dest('dist/bower_components/'));
+});
+
+gulp.task('watch', function() {
+
+	browserSync.init({
+		server: "dist/"
+	});
+
+	gulp.run(['default']);
+	gulp.watch('app/**/*', ['default']);
+	gulp.watch(['dist/**/*','!dist/bower_components/**/*']).on('change', browserSync.reload);
+});
+
+gulp.task('clean', function() {
+	gulp.src('dist/**/*.*')
+		.pipe(clean({force: true}));
+});
+
+gulp.task('scss', function() {
+	gulp.src('app/**/*.scss')
+		.pipe(plumber())
+		.pipe(scss().on('error', scss.logError))
+		.pipe(gulpif(production, sourcemaps.init()))
+		.pipe(gulpif(production, minifyCss()))
+		.pipe(gulpif(production, sourcemaps.write('maps/')))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('es6', function() {
+	gulp.src('app/**/*.es6')
+		.pipe(plumber())
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulpif(production, sourcemaps.init()))
+		.pipe(gulpif(production, uglify()))
+		.pipe(gulpif(production, sourcemaps.write('maps/')))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('ejs', function() {
+	gulp.src('app/**/*.ejs')
+		.pipe(plumber())
+		.pipe(ejs())
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['scss','es6','ejs','static']);

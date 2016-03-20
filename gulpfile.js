83714d78dc495582a1684b35536982644da0e3b9
neungkl <kosatelim@gmail.com>
2016-03-20 11:18:27 +0700

    Finish 1 step

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,31 @@ var gulpif = require('gulp-if');
 var minifyCss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 var production = true;
+
+gulp.task('move', function() {
+    gulp.src([
+		'app/**/*',
+        '!app/bower_components/**/*',
+		'!app/**/*.scss',
+		'!app/**/*.es6',
+		'!app/**/*.ejs',
+		'!app/**/*.tmp',
+        '!app/img/**/*'
+	], {base: 'app'})
+		.pipe(gulp.dest('dist/'));
+})
 
 gulp.task('static', function() {
 	var bowerList = [
 		'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/pure/pure-min.css'
+        'bower_components/semantic/dist/semantic.min.css',
+        'bower_components/semantic/dist/semantic.min.js',
+        'bower_components/semantic/dist/themes/default/assets/**/*'
+        
 	];
 
 	gulp.src([
@@ -24,9 +42,18 @@ gulp.task('static', function() {
 		'!app/**/*.scss',
 		'!app/**/*.es6',
 		'!app/**/*.ejs',
-		'!app/**/*.tmp'
+		'!app/**/*.tmp',
+        '!app/img/**/*'
 	], {base: 'app'})
 		.pipe(gulp.dest('dist/'));
+   
+    gulp.src('app/img/**/*')
+        .pipe(imagemin({
+            progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img/'));
 
 	gulp.src(bowerList.map(function(val) { return 'app/' + val }), {base : 'app/bower_components/'})
 		.pipe(gulp.dest('dist/bower_components/'));
@@ -39,7 +66,7 @@ gulp.task('watch', function() {
 	});
 
 	gulp.run(['default']);
-	gulp.watch('app/**/*', ['default']);
+	gulp.watch('app/**/*', ['scss','es6','ejs','move']);
 	gulp.watch(['dist/**/*','!dist/bower_components/**/*']).on('change', browserSync.reload);
 });
 
